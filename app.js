9ee2ec9091c88bb0224780337b63290b975e9b86
fashionblocks <fashionblocks.io@gmail.com>
2022-04-07 20:38:19 +0800

    init

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const utils = require('./utils');
+const md = require('markdown-it')();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'index',
+    partialsDir: __dirname + '/views/partials/',
+  }),
+);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  const readme = md.render(utils.getReadme());
+
+  res.render('main', {
+    ...utils.getNavAttributes(),
+    readme
+  });
+});
+
+app.get('/:route', (req, res) => {
+  const query = req.query;
+  console.log(query['tokenId']);
+  res.render('piece', {
+    ...utils.getNavAttributes(),
+    scriptName: `${req.params.route}.js`,
+    tokenId: query['tokenId'],
+    hash: query['hash'].toString()
+  });
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('Not found');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
